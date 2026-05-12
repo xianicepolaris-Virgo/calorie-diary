a4c153378434acc40eb6d6876e2edc8fe07bddf4
xianxian <63961305+xianicepolaris-Virgo@users.noreply.github.com>
2026-05-13 01:20:28 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.SUPABASE_CONFIG = {
-  url: "https://tkcyvamtooevqzqazviu.supabase.co/rest/v1/",
+  url: "https://tkcyvamtooevqzqazviu.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrY3l2YW10b29ldnF6cWF6dml1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1OTYwOTMsImV4cCI6MjA5NDE3MjA5M30.dzF3N7uiIgfZ2fv5TbkmrH9F4Kvrkhge4Iv_1TtyutU",
   table: "health_diary",
   rowId: "default",
